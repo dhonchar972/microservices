4a83b2180aeb6bea97c0d9d815180499b6e59923
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-11-08 00:33:20 +0200

    new chapters (#2)
    * added chapter one(simplest realisation)
    
    * added new java chapter(three)

---
+++ java/chapter03/microservices/src/test/java/ua/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,17 @@
+package ua.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Dmytro Honchar <dmytro.honchar972@gmail.com>
+ * Date: 11/7/2022
+ */
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
