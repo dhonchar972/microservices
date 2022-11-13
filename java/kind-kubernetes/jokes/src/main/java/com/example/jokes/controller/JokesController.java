0452761cd4b7de308700802ca9251f82936f395d
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-11-13 18:57:30 +0200

    kind project added(java)

---
+++ java/kind-kubernetes/jokes/src/main/java/com/example/jokes/controller/JokesController.java
@@ -0,0 +1,23 @@
+package com.example.jokes.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.jusaf.jokes.client.ChuckNorrisJokesClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/jokes")
+@RequiredArgsConstructor
+public class JokesController {
+
+    private final ChuckNorrisJokesClient client;
+
+    @GetMapping("random")
+    public Map<String, String> getRandomJoke() {
+        return Map.of("joke", client.getRandomJoke());
+    }
+}
