d712e13f621dc11b1d6a1f7ae1070f0f42de6bd6
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-11-23 00:25:25 +0200

    added jwt auth

--- python/poetry-project/src/auth.py
+++ python/poetry-project/src/auth.py
@@ -1,8 +1,9 @@
 import json
-from src.constants.http_status_codes import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 import validators
+from flask_jwt_extended import create_access_token, create_refresh_token
 from src.db import User, db
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
@@ -44,6 +45,33 @@ def register():
             'email': email
         }
     }), HTTP_201_CREATED
+
+
+@app.post("/login")
+def login():
+    email = request.json.get('email', ''),
+    password = request.json.get('password', '')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        is_pass_correct = check_password_hash(user.password, password)
+
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_refresh_token(identity=user.id)
+
+            return jsonify({
+                'user':{
+                    'refresh':refresh,
+                    'access':access,
+                    'username':user.username,
+                    'email':user.email,
+                }
+            }), HTTP_200_OK
+
+    return jsonify({'error': 'Wrong credential'}), HTTP_401_UNAUTHORIZED
+
 
 @auth.get("/me")
 def me():
